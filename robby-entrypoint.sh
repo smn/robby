b8d1b3346c5d777b8d348569e89522d19df65dd5
Simon de Haan <simon@praekeltfoundation.org>
2016-04-10 22:03:53 -0700

    print redis-uri

--- robby-entrypoint.sh
+++ robby-entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
 
+echo "Starting robby with redis://${REDIS_HOST:-127.0.0.1}:${REDIS_PORT:-6379}/${REDIS_DB:-1}"
 python `which robby` \
     --redis-uri redis://${REDIS_HOST:-127.0.0.1}:${REDIS_PORT:-6379}/${REDIS_DB:-1} \
     --interface 0.0.0.0 \

af6298c4eb9c7852da87916693675baa5e584c62
Simon de Haan <simon@praekelt.org>
2018-11-01 13:28:18 +0100

    version bump for travis fix

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open('requirements-dev.txt') as req_file:
 
 setup(
     name='robby',
-    version='0.5.0',
+    version='0.5.1',
     description="Robby",
     long_description=readme,
     author="Simon de Haan",
